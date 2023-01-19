c00a20d916bc7008374a16e75ef2ad4a8274f28e
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-01-19 10:37:05 +0100

    småendringer i modellen

--- model.js
+++ model.js
@@ -1,100 +1,98 @@
 const root = document.getElementById("root");
 
 const model = {
-  // app ↓
-  state: "homePage",
-  curentUser: undefined,
-
-  // Input ↓
-  bookingData: {
-    firstName: "",
-    lastName: "",
-    email: "",
-    adress: "",
-    zipCode: "",
-
-    days: [],
+  app: {
+    state: "homePage",
+    curentUser: null,
   },
 
-  shoppingCart: [],
+  inputs: {
+    bookingData: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      adress: "",
+      zipCode: "",
 
-  loginData: {
-    username: "",
-    pasword: "",
+      days: [],
+    },
+
+    loginData: {
+      username: "",
+      pasword: "",
+    },
+
+    shoppingCart: [],
+
+    shoppingFilters: [],
   },
 
-  filters: [],
+  data: {
+    portfolioArticles: [
+      {
+        title: "Mauertua barnehage",
+        img: "ing/mauertua barnehage.png",
+        content: [
+          { text: "i dette prosjektet..." },
+          { img: "img/mauertua barnehage.png" },
+          { text: "mer tekst og ting..." },
+        ],
+        articleId: 0,
+      },
+      {
+        title: "Annen barnehage",
+        img: "ing/annen barnehage.png",
+        content: [
+          { text: "i dette prosjektet..." },
+          { img: "img/annen barnehage.png" },
+          { text: "mer tekst og ting..." },
+        ],
+        articleId: 1,
+      },
+    ],
+    storeArticles: [
+      {
+        title: "Kurs i mattematikk",
+        img: "img/kurs i matteematikk.png",
+        description: "dette er ett flott produkt..",
+        category: ["Utdanningsmatreale", "Mattematikk"],
+        storeId: 0,
+        price: 24543,
+      },
+      {
+        title: "Kurs i naturfag",
+        img: "img/kurs i naturfag.png",
+        description: "dette er ett flott produkt..",
+        category: ["Utdanningsmatreale", "Naturfag"],
+        storeId: 1,
+        price: 24543,
+      },
+      {
+        title: "Hettegenser",
+        img: "img/hettegenser.png",
+        description: "dette er ett flott produkt..",
+        category: ["Hettegenser", "Klær"],
+        colors: [{ text: "Svart", css: "black" }],
+        storeId: 2,
+        price: 999,
+      },
+    ],
 
-  // Data ↓
-  portfolioArticles: [
-    {
-      title: "Mauertua barnehage",
-      img: "ing/mauertua barnehage.png",
-      content: [
-        { text: "i dette prosjektet..." },
-        { img: "img/mauertua barnehage.png" },
-        { text: "mer tekst og ting..." },
-      ],
-      articleId: 0,
-    },
-    {
-      title: "Annen barnehage",
-      img: "ing/annen barnehage.png",
-      content: [
-        { text: "i dette prosjektet..." },
-        { img: "img/annen barnehage.png" },
-        { text: "mer tekst og ting..." },
-      ],
-      articleId: 1,
-    },
-  ],
-  storeArticles: [
-    {
-      title: "Kurs i mattematikk",
-      img: "img/kurs i matteematikk.png",
-      description: "dette er ett flott produkt..",
-      category: ["Utdanningsmatreale", "Mattematikk"],
-      storeId: 0,
-      price: 24543,
-    },
-    {
-      title: "Kurs i naturfag",
-      img: "img/kurs i naturfag.png",
-      description: "dette er ett flott produkt..",
-      category: ["Utdanningsmatreale", "Naturfag"],
-      storeId: 1,
-      price: 24543,
-    },
-    {
-      title: "Hettegenser",
-      img: "img/hettegenser.png",
-      description: "dette er ett flott produkt..",
-      category: ["Hettegenser", "Klær"],
-      colors: [{ text: "Svart", css: "black" }],
-      storeId: 2,
-      price: 999,
-    },
-  ],
+    users: [
+      {
+        name: "Heine",
+        userId: 0,
+        pasword: "hemmelig",
+      },
+    ],
 
-  users: [
-    {
-      name: "Heine",
-      userId: 0,
-    },
-  ],
-
-  orders: [
-    {
-      userId: 0,
-      storeIds: [0, 2],
-      orderDate: "18.01.2023",
-    },
-  ],
-  calendar: [{ date: "28.01.2023" }],
+    orders: [
+      {
+        userId: 0,
+        storeIds: [0, 2],
+        orderDate: "18.01.2023",
+      },
+    ],
+    calendar: [{ date: "28.01.2023" }],
+  },
 };
-
-if (model.curentUser) {
-  console.log("kake");
-} else {
-  console.log("kake2");
-}
